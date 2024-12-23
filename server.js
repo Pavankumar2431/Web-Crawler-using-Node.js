efafde9e173ebdf605fd7e0acdcaadb11db26060
Pavankumar2431 <mallipavankumar99@gmail.com>
2024-12-23 11:38:15 +0530

    s

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-
+app.use(express.json());
 // Get the directory name from import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
